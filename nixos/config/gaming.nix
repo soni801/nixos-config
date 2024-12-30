6bbf831663ce16ffb95799f78034c066ee9a51eb
Soni <soni@recalstudios.net>
2024-12-30 21:23:00 +0100

    fix: wrap modrinth to fix segfault

--- nixos/config/gaming.nix
+++ nixos/config/gaming.nix
@@ -8,6 +8,11 @@
     tetrio-desktop
   ];
 
+  # Fix Modrinth segfault on launch
+  nixpkgs.overlays = [
+    ( import ../../overlays/modrinth-app.nix )
+  ];
+
   # Programs
   programs.steam.enable = true;
 }
---
+++ overlays/modrinth-app.nix
@@ -0,0 +1,11 @@
+final: prev: {
+  modrinth-app = prev.modrinth-app.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs or [ ] ++ [ prev.pkgs.makeWrapper ];
+    postInstall = oldAttrs.postInstall or "" + ''
+      wrapProgram $out/bin/ModrinthApp \
+        --set WEBKIT_DISABLE_DMABUF_RENDERER 1 \
+	--set LIBGL_ALWAYS_SOFTWARE 1 \
+	--set GDK_BACKEND x11
+    '';
+  });
+}
