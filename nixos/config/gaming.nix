dbc17aeb529e3909f91ea77d82efabe6a3b3a7a4
Soni <soni@recalstudios.net>
2025-01-30 17:28:14 +0100

    chore: remove unused modrinth stuff

--- nixos/config/gaming.nix
+++ nixos/config/gaming.nix
@@ -4,9 +4,6 @@
   # Packages
   environment.systemPackages = with pkgs; [ clonehero polymc tetrio-desktop ];
 
-  # Fix Modrinth segfault on launch
-  nixpkgs.overlays = [ (import ../../overlays/modrinth-app.nix) ];
-
   # Programs
   programs.steam.enable = true;
 }
