cfcf96453f8e9370a743b52021265dbe8c109f45
Soni <soni@recalstudios.net>
2025-11-10 21:16:30 +0100

    fix: finally install modrinth properly

--- nixos/config/gaming.nix
+++ nixos/config/gaming.nix
@@ -5,8 +5,22 @@
   environment.systemPackages = with pkgs; [
     clonehero
     tetrio-desktop
+
+    # Modrinth requires a silly fix
+    # https://github.com/NixOS/nixpkgs/issues/359820#issuecomment-2601110696
+    (modrinth-app.overrideAttrs (oldAttrs: {
+      buildCommand = 
+	''
+	  gappsWrapperArgs+=(
+	    --set GDK_BACKEND x11
+	    --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+	  )
+	''
+	+ oldAttrs.buildCommand;
+    }))
   ];
 
   # Programs
   programs.steam.enable = true;
+
 }
