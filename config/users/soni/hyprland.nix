a32fb1d103bc583edf9b38554e75c0d2a8fb5594
Soni <soni@recalstudios.net>
2025-11-17 18:40:20 +0100

    chore: remove unused envvars

--- config/users/soni/hyprland.nix
+++ config/users/soni/hyprland.nix
@@ -202,12 +202,5 @@
       no_donation_nag = true;
     };
   };
-
-  # Environment variables
-  home.sessionVariables = {
-    XCURSOR_SIZE = "24";
-    HYPRCURSOR_SIZE = "24";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-  };
 }
 
