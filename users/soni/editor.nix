7574e3010df6401355af18d6953ae9f932ea7ec3
Soni <soni@recalstudios.net>
2025-11-25 23:12:42 +0100

    feat(editor): add nvim config

--- users/soni/editor.nix
+++ users/soni/editor.nix
@@ -5,8 +5,22 @@
     inputs.nix4nvchad.homeManagerModule
   ];
 
-  programs.nvchad.enable = true;
-
+  programs.nvchad = {
+    enable = true;
+    extraConfig = ''
+      vim.opt.colorcolumn = "80"
+      vim.opt.list = true
+      vim.opt.listchars = {
+        space = "\u{22C5}",
+        tab = "\u{2192}\u{2002}"
+      }
+    '';
+    chadrcConfig = ''
+      local M = {}
+      M.ui = {theme = 'catppuccin'}
+      return M
+    '';
+  };
   # Set nvim as preferred editor
   home.sessionVariables.EDITOR = "nvim";
 }
