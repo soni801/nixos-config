83496ebe033ba6485878123c97c1029f480f9f70
Soni <soni@recalstudios.net>
2025-12-06 14:18:56 +0100

    fix(nix): increase download buffer size for nix

--- modules/nix-options.nix
+++ modules/nix-options.nix
@@ -9,7 +9,10 @@
   };
 
   # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    download-buffer-size = 524288000;
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
