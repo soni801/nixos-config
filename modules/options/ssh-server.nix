41275b0f9efbc401f1aeb703bcf503e4f2c5cd15
Soni <soni@recalstudios.net>
2026-03-26 09:41:20 +0100

    feat(ssh-server): disable root login, X11 forwarding, and add session timeouts

--- modules/options/ssh-server.nix
+++ modules/options/ssh-server.nix
@@ -5,7 +5,10 @@
     enable = true;
     settings = {
       AllowUsers = [ "samuel" ];
+      PermitRootLogin = "no";
+      X11Forwarding = false;
+      ClientAliveInterval = 300;
+      ClientAliveCountMax = 2;
     };
   };
 }
-
